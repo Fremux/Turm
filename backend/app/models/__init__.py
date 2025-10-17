9fc4f446b5aabdae70f0c9d1512f9f4216adfee3
Alexander Mokin <alejmokin@gmail.com>
2025-10-18 00:49:01 +0300

    Add user tables

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
 import models.base
 import models.organizations
 import models.task
+import models.user
---
+++ backend/app/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import String, TIMESTAMP
+from sqlalchemy.orm import mapped_column, Mapped
+from datetime import datetime
+from typing import List
+from sqlalchemy.dialects.postgresql import JSONB
+from models.base import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True, auto_increment=True)
+    name: Mapped[str] = mapped_column(String)
+    surname: Mapped[str] = mapped_column(String)
+    email: Mapped[str] = mapped_column(String)
+    organization: Mapped[str] = mapped_column(String)
+    role: Mapped[str] = mapped_column(String)
+    sector: Mapped[str] = mapped_column(String)
+    tags: Mapped[List[str]] = mapped_column(JSONB, nullable=False, server_default="{}")
+    birth_date: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+    description: Mapped[str] = mapped_column(String)
